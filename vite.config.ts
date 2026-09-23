7972c205a81ea13fda497bebdbcb67ba31539840
sayildiz <25555261+sayildiz@users.noreply.github.com>
2026-09-23 22:42:24 +0200

    set relative base path for assets

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [svelte()],
   optimizeDeps: {
     exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
